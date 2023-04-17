ad980cceef811cdb4abd1e5437053a0cbe0dc953
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-17 20:07:23 +0330

    add new book

--- app/book.py
+++ app/book.py
@@ -1,32 +1,69 @@
 from fastapi import Depends
 from fastapi.responses import JSONResponse
+import json
+from bson import ObjectId
+
 
 from .database import get_db
 class Book:
     """
         CRUD operations in books in database
     """
+    fields = [
+        "title",
+        "author",
+        "description",
+        "published_year",
+        "publisher"
+    ]
+    
     def __init__(self, db = Depends(get_db)) -> None:
         self.db = db
         
         
-    def get_book_by_id(self, id: int):
+    def get_book_by_id(self, id: str):
         book = self.db.books.find_one({
-            "id": id
+            "_id": ObjectId(id)
         })
         if not book:
             return self.not_found()
-        return book
+        return str(book)
     
-    
+
     def get_all_books(self):
         books = []
         for book in self.db.books.find():
             books.append(book)
-        return books
+        return str(books)
+    
+    
+    def add_new_book(self, book):
+        if not all(key in self.fields for key in book.keys()):
+            return self.unproessable()
         
-        
+        response = None
+        try:
+            response = self.db.books.insert_one(book)
+        except Exception:
+            return self.error_insert()
+        return {
+            "detail": "book created successfuly.",
+        }
+    
+    
     def not_found(self):
         return JSONResponse({
             "detail": "book not found."
         }, 404)
+        
+    
+    def error_insert(self):
+        return JSONResponse({
+            "detail": "error in insert"
+        }, 409)
+        
+
+    def unproessable(self):
+        return JSONResponse({
+            "detail": "Missing required keys in request data"
+        }, 422)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pymongo import MongoClient
-import os
+from fastapi import FastAPI, Depends, Body
+from bson import ObjectId
 
 from app.database import get_db
 from app.book import Book
@@ -17,8 +16,9 @@ async def get_all_books(db = Depends(get_db)):
     return book_instance.get_all_books()
 
 
+
 @app.get("/books/{book_id}")
-async def get_book_by_id(book_id: int, db = Depends(get_db)):
+async def get_book_by_id(book_id: str, db = Depends(get_db)):
     """
         Retrive a book with id
     """
@@ -26,12 +26,15 @@ async def get_book_by_id(book_id: int, db = Depends(get_db)):
     return book_instance.get_book_by_id(book_id)
 
 
+
 @app.post("/books")
-async def add_a_new_book():
+async def add_a_new_book(book = Body(), db = Depends(get_db)):
     """
         Create a new book
     """
-    pass
+    book_instance = Book(db)
+    return book_instance.add_new_book(book)
+
 
 
 @app.put("/books/{book_id}")
