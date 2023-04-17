caccb4381c50c29cd253255030d7d77a87f3367a
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-17 16:49:58 +0330

    get all + get book by id

--- app/book.py
+++ app/book.py
@@ -1,8 +1,32 @@
+from fastapi import Depends
+from fastapi.responses import JSONResponse
+
+from .database import get_db
 class Book:
     """
         CRUD operations in books in database
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, db = Depends(get_db)) -> None:
+        self.db = db
+        
+        
+    def get_book_by_id(self, id: int):
+        book = self.db.books.find_one({
+            "id": id
+        })
+        if not book:
+            return self.not_found()
+        return book
     
     
+    def get_all_books(self):
+        books = []
+        for book in self.db.books.find():
+            books.append(book)
+        return books
+        
+        
+    def not_found(self):
+        return JSONResponse({
+            "detail": "book not found."
+        }, 404)
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, Depends
 from pymongo import MongoClient
 import os
 
 from app.database import get_db
+from app.book import Book
 
 app = FastAPI()
 
 
 @app.get("/books")
-async def get_all_books():
+async def get_all_books(db = Depends(get_db)):
     """
         Retrive all books
     """
-    pass
+    book_instance = Book(db)
+    return book_instance.get_all_books()
 
 
 @app.get("/books/{book_id}")
-async def get_book_by_id():
+async def get_book_by_id(book_id: int, db = Depends(get_db)):
     """
         Retrive a book with id
     """
-    pass
+    book_instance = Book(db)
+    return book_instance.get_book_by_id(book_id)
+
 
 @app.post("/books")
 async def add_a_new_book():
@@ -45,4 +48,3 @@ async def delete_a_book():
         Delete a book by id
     """
     pass
-    
